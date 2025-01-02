19078cecbb1ff7070b21ead8b1501212846c7dc2
junhyeok <whdkwhdk90@naver.com>
2025-01-02 22:12:05 +0900

    수정

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -8,9 +8,9 @@ useSeoMeta({
 
 <template>
   <div class="space-y-6">
-    <NuxtImg src="/avatar.png" alt="Lee JunHyeok"
-      class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"
-      sizes="48px sm:64px" placeholder format="webp" />
+<!--    <NuxtImg src="/avatar.png" alt="Lee JunHyeok"-->
+<!--      class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"-->
+<!--      sizes="48px sm:64px" placeholder format="webp" />-->
     <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
       안녕하세요!
     </h1>
