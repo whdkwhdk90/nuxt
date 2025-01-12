f482e57accc53ca32b2403e79fad4756d65c14bc
junhyeok <whdkwhdk90@naver.com>
2025-01-12 22:04:38 +0900

    intro수정

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: "Lee JunHyeok",
+  title: "introduce",
   description:
       "I'm Fayaz, your friendly neighborhood software, product engineer and designer from Bengaluru, India. I specialize in building web applications and sites using Javascript, React, Vue & Node.",
 });
@@ -8,7 +8,7 @@ useSeoMeta({
 
 <template>
   <div class="space-y-6">
-<!--    <NuxtImg src="/avatar.png" alt="Lee JunHyeok"-->
+<!--    <NuxtImg src="/avatar.png" alt="introduce"-->
 <!--      class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"-->
 <!--      sizes="48px sm:64px" placeholder format="webp" />-->
     <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
